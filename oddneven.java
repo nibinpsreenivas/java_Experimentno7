8149ecd1ffefceb14ec468593c89e60b16f2e66b
nibinpsreenivas <96453329+nibinpsreenivas@users.noreply.github.com>
2022-02-20 11:42:07 +0530

    Add files via upload

---
+++ oddneven.java
@@ -0,0 +1,40 @@
+import java.util.*;
+ class thread1 extends Thread
+  { 
+     //System.out.println("the odd numbers are:");
+      public void run()
+       {
+         for(int i=1;i<100;i++)
+           if(i%2!=0)
+            System.out.println(i);
+       } 
+  }
+  class thread2 extends Thread
+  { 
+     //System.out.println("the even numbers are:");
+      public void run()
+       {
+         for(int i=1;i<100;i++)
+           if(i%2==0)
+            System.out.println(i);
+       } 
+  }
+class oddneven
+{
+  public static void main(String args[])
+   {
+     thread1 t1 = new thread1();  
+     thread2 t2 = new thread2();  
+      System.out.println("the odd numbers are:");
+     t1.start();
+    try{
+         t1.join();
+       }catch(InterruptedException e)
+         {
+           System.out.println(e);
+         }
+             
+    System.out.println("the even numbers are:");
+     t2.start();
+    }
+}
